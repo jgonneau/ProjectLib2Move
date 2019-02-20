9ef57d0f075c8e5d13912c368cad04376cc6af5c
jgonneau <jgonneau@github.com>
2019-02-20 15:07:25 +0100

    update themes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,6 +12,13 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        //Redirection si authentifié admin
+        if ($this->get('security.authorization_checker')->isGranted('ROLE_ADMIN'))
+        {
+            return $this->render('admin/dashboard.html.twig', [
+                'controller_name' => 'AdminController',
+            ]);
+        }
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'error' => ''
