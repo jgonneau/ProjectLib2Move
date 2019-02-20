56b613d89a20a0a1caf40ec589e41fd650181314
Presley Lupon <presleylupon@gmail.com>
2019-02-20 10:05:25 +0100

    vehicle

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccessController extends AbstractController
+class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->render('access/index.html.twig', [
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
             'error' => ''
         ]);
     }
---
+++ src/Form/VehicleType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Vehicle;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VehicleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('typeOfVehicle')
+            ->add('brand')
+            ->add('model')
+            ->add('serialNumber')
+            ->add('color')
+            ->add('numberplate')
+            ->add('kilometers')
+            ->add('dateOfPurchase')
+            ->add('buyingPrice')
+            ->add('createdAt')
+            ->add('createdBy')
+            ->add('modifiedAt')
+            ->add('modifiedBy')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Vehicle::class,
+        ]);
+    }
+}
