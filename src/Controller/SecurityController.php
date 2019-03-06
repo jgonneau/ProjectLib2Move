e7d0c65afadd31f18d12a7830a3097daba904320
jgonneau <jgonneau@github.com>
2019-03-06 13:15:03 +0100

    add registering part

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,26 +2,70 @@
 
 namespace App\Controller;
 
+use App\Entity\Role;
 use App\Entity\User;
+use App\Entity\UserRole;
 use App\Form\LoginUserType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\RegisterUserType;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/security", name="security")
+     * @Route("/register", name="register")
      */
     public function index()
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = new User();
+        $userRole = new UserRole();
+
+        //L'on définit le rôle USER par défaut pour les utilisateurs
+        $roleName = 'ROLE_USER';
+
+        //L'on recupere et verifie si le rôle existe déjà dans la base de données
+        $role = $entityManager->getRepository(Role::class)->findOneBy(['nomRole' => $roleName]);
+        if ($role)
+        {
+            //L'on permet à l'utilisateur d'acceder au statut de ROLE_USER
+            $userRole->setRole($role);
+            $userRole->setUser($user);
+        }
+        else
+        {
+            //L'on crée le role USER si non existant dans la base de données
+            $entityManager->persist($userRole);
+            $entityManager->flush();
+
+            //L'on recupere et verifie en même temps que le role existe maintenant
+            $role = $entityManager->getRepository(Role::class)->findOneBy(['nomRole' => $roleName]);
+        }
+
+        //Definition du formulaire
+        $form = $this->createFormBuilder($user)
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('birthday', BirthdayType::class)
+            ->add('phoneNumber', TextType::class)
+            ->getForm();
+
+        $form = $this->createForm(RegisterUserType::class, $user);
+
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 
     /**
-     * @Route("/security/login", name="login")
+     * @Route("/login", name="login")
      * @param AuthenticationUtils $authenticationUtils
      * @return \Symfony\Component\HttpFoundation\Response
      */
---
+++ src/Form/RegisterUserType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class RegisterUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('email', EmailType::class)
+            ->add('birthday', BirthdayType::class)
+            ->add('phoneNumber')
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options' => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password')
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
