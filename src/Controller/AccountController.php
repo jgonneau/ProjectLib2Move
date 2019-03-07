72208b57f015ad0d2fc04f63fe5e19c23026e641
jgonneau <jgonneau@github.com>
2019-03-07 09:01:58 +0100

    Mise en forme - Account User

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,15 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\VehicleRepository;
 
 class AccountController extends AbstractController
 {
     /**
      * @Route("/account", name="account")
      */
-    public function index()
+    public function index(VehicleRepository $VR)
     {
+        //NOTE: A remplacer VehiclRepo par OfferRepository ..
+        $all_offers = $VR->findAll();
+
         return $this->render('account/index.html.twig', [
+            'offers' => $all_offers
         ]);
     }
 }
